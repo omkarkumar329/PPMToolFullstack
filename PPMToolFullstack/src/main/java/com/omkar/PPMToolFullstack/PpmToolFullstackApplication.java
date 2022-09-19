b327269976d2183d39bbafaceda6dde4714648ea
Omkar Kumar <Omkarkumar329@gmail.com>
2022-09-19 23:11:27 +0530

    first project

---
+++ PPMToolFullstack/src/main/java/com/omkar/PPMToolFullstack/PpmToolFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.omkar.PPMToolFullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullstackApplication.class, args);
+	}
+
+}
---
+++ PPMToolFullstack/src/test/java/com/omkar/PPMToolFullstack/PpmToolFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omkar.PPMToolFullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

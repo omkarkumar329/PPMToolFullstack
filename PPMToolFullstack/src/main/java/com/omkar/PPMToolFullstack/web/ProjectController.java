e480968f9b1ccfd0fda1c93a2ed9c89090a32806
Omkar Kumar <Omkarkumar329@gmail.com>
2022-09-21 05:13:13 +0530

    Completed Validation But Unique=true is not working, For that we will look into exception

--- PPMToolFullstack/src/main/java/com/omkar/PPMToolFullstack/web/ProjectController.java
+++ PPMToolFullstack/src/main/java/com/omkar/PPMToolFullstack/web/ProjectController.java
@@ -1,14 +1,21 @@
 package com.omkar.PPMToolFullstack.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.omkar.PPMToolFullstack.Domain.Project;
+import com.omkar.PPMToolFullstack.Services.MapValidationErrorService;
 import com.omkar.PPMToolFullstack.Services.ProjectService;
 
 @RestController
@@ -18,10 +25,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    MapValidationErrorService mapValidationErrorService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+    	ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
 }
